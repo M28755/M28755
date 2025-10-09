44724711d84eb249f705d07f4d3f6815ad382959
M28755 <omondimichael3087@gmail.com>
2025-10-09 08:12:25 +0300

    Add files via upload

---
+++ for_loop.c
@@ -0,0 +1,24 @@
+// program to print number 100 to 50 in discending order
+/*
+NAME:       OMONDI MICHAEL OTIENO
+REG NO:     PA106/G/28755/25
+DATE:       8\10\2025
+DESCRIPTION:C Program to print numbers from 100 to 50
+*/
+
+#include <stdio.h>
+
+int main()
+{   // variable
+	int i;
+	
+	//   start  ; condition ; step
+	for(i = 100 ; i >= 50 ; i--)
+	{
+		
+		printf("\n %d", i);
+		
+	}printf("\n\n");
+	
+	return 0;
+}
